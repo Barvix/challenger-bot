1b46d4e5a810593146404ad4e43e4d8c9851c2f6
Barvix <barvixinc@gmail.com>
2018-06-24 01:48:36 -0400

    Update ChallengeBot.py

--- ChallengeBot.py
+++ ChallengeBot.py
@@ -200,10 +200,10 @@ async def timeleft(ctx):
     BUCKET_NAME = 'cloud-cube' # replace with your bucket name
     KEY = "cvxsngshjp1h/"+challenge_file # replace with your object key
 
-    #s3 = boto3.resource('s3')
+    xs3 = boto3.resource('s3')
 
     try:
-        s3.Bucket(BUCKET_NAME).download_file(KEY, challenge_file)
+        xs3.Bucket(BUCKET_NAME).download_file(KEY, challenge_file)
     except botocore.exceptions.ClientError as e:
         if e.response['Error']['Code'] == "404":
             print("The object does not exist.")
