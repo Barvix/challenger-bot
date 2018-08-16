3dad13b7adbe9ee9e9a21e5b92098ba44494a82b
Barvix <barvixinc@gmail.com>
2018-08-15 22:16:31 -0400

    Update ChallengeBot.py

--- ChallengeBot.py
+++ ChallengeBot.py
@@ -105,6 +105,7 @@ async def on_message(message):
                  if "feedback" not in [y.name.lower() for y in message.author.roles]:
                         #await bot.send_message(message.channel , "Hey now <@"+str(message.author.id)+">, you must first give **QUALITY** feedback before asking for some. If you think you got this message in error, please contact a mod or admin. If you're not sure what quality feedback looks like, look here: <https://i.imgur.com/ZXAHtQw.jpg>")
                         #await bot.delete_message(message)
+                        donothin = message.channel
                  if "feedback" in [y.name.lower() for y in message.author.roles]:
                     return
             if "🎧🎧🎧feedback giver🎧🎧🎧" in [y.name.lower() for y in message.author.roles]:
