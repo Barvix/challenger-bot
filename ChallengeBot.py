054db5859b9c238165f158ebbfd0031cbac099dd
Barvix <barvixinc@gmail.com>
2018-12-18 23:28:32 -0500

    Update ChallengeBot.py

--- ChallengeBot.py
+++ ChallengeBot.py
@@ -140,7 +140,7 @@ async def on_message(message):
         if ( (message.channel.id == "472838612119978034") and ("https://" in message.content or "soundcloud.com" in message.content)):
             if "🎧🎧🎧feedback giver🎧🎧🎧" not in [y.name.lower() for y in message.author.roles]:
                  if "feedback" not in [y.name.lower() for y in message.author.roles]:
-                        await bot.send_message(message.channel , "Hey now <@"+str(message.author.id)+">, you must first give **QUALITY** feedback before asking for some. If you think you got this message in error, please contact a mod or admin. If you're not sure what quality feedback looks like, look here: <https://i.imgur.com/ZXAHtQw.jpg>\nIf you are here to just post tracks and not really interact with the community or return to feedback to other producers, then this community is not the one for you.")
+                        await bot.send_message(message.channel , "Hey now <@"+str(message.author.id)+">, you must first give **QUALITY** feedback before asking for some. If you think you got this message in error, please contact a mod or admin. If you're not sure what quality feedback looks like, look here: <https://i.imgur.com/ZXAHtQw.jpg>\nIf you are here to just post tracks and not really interact with the community or return feedback to other producers, then this community is not the one for you.")
                         await bot.delete_message(message)
                         donothin = message.channel
                  if "feedback" in [y.name.lower() for y in message.author.roles]:
