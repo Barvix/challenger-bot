b17d2ca2573ca0a202bd7fe9fbf19a2fb30b51df
Barvix <barvixinc@gmail.com>
2019-04-06 17:28:25 -0400

    Update ChallengeBot.py

--- ChallengeBot.py
+++ ChallengeBot.py
@@ -174,7 +174,7 @@ async def on_message(message):
         
         if (fb == "428972162779578368"):
             if ("feedback" in message.content.lower()):
-                rand_feedback = random.randint(0, 14)
+                rand_feedback = random.randint(0, 15)
                 feedback_message = "test text"
                 if (rand_feedback == 0): feedback_message = "Yo fam, this shit bangs in the whip. Like as soon as I play this in my Honda Pilot, the whip bangs bro, and not like the porn studios. I fuck with it."
                 if (rand_feedback == 1): feedback_message = "Not gonna lie fam, this shit weak. The fuck is going on with that bass? Is it outta tune? Just a weak melody? A lame ass bass I banged in my whip 500000000 times? fuck outta here with this lame shit"
@@ -191,6 +191,7 @@ async def on_message(message):
                 if (rand_feedback == 12): feedback_message = "Yo i spit out my Arizona tea as soon as that bass dropped man. Kinda came a little, too. Now that last part might have to do with the hentai I was watching, but I don't think so because I don't even like hentai."
                 if (rand_feedback == 13): feedback_message = "Can i have sex with this beat? Cus this beat, especially the low ends of it man, are fine as hellllllll"
                 if (rand_feedback == 14): feedback_message = "This mix would be balanced if my monitors only had tweeters"
+                if (rand_feedback == 15): feedback_message = "This one could use some Waves Abbey Road Ultra de-distorter V4 to remove some overtones"
                 chn = bot.get_channel("560511832322736138")
                 await bot.send_message(chn, feedback_message)
         
