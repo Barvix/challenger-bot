d240c3213892f39e2f95f9ae02f297989a10b541
Barvix <barvixinc@gmail.com>
2020-03-24 15:52:07 -0400

    Update ChallengeBot.py

--- ChallengeBot.py
+++ ChallengeBot.py
@@ -295,6 +295,16 @@ async def on_message(message):
                         else:
                             return
         
+        if ( (message.channel.id == "472402996378992650") and ("https://" in message.content or "soundcloud.com" in message.content or "http://" in message.content)):
+            if "feedback" not in [y.name.lower() for y in message.author.roles]:
+                await bot.send_message(message.channel , "Hey now <@"+str(message.author.id)+">, in order to post here you must have the feedback role, and it looks like you don't have it. To get the feedback role you must give someone feedback first. Please remember this is a **feedback** channel, not a promotion channel.")
+                        await bot.delete_message(message)
+        
+        if (message.channel.id == "472402996378992650" and ("http" not in message.content.lower())):    
+            if any(fbr in message.content.lower() for fbr in fb_list):
+                role = discord.utils.get(message.server.roles, name="Feedback")
+                await bot.add_roles(message.author, role)
+        
         if ( (message.channel.id == "560511832322736138") and ("https://" in message.content or "soundcloud.com" in message.content or "http://" in message.content)):
             if "🎧🎧🎧quality feedback giver🎧🎧🎧" not in [y.name.lower() for y in message.author.roles]:
                  if "feedback" not in [y.name.lower() for y in message.author.roles]:
@@ -317,6 +327,7 @@ async def on_message(message):
                             
             if "🎧🎧🎧quality feedback giver🎧🎧🎧" in [y.name.lower() for y in message.author.roles]:
                 return
+            
         if (message.channel.id == "560511832322736138" and ("http" not in message.content.lower())):    
             if any(fbr in message.content.lower() for fbr in fb_list):
                 role = discord.utils.get(message.server.roles, name="Feedback")
